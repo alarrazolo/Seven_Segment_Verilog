2bbae8e2bec3ebe2da5fee2d17273c6f98f9eef8
DESKTOP-EK1IM0Q\alarrazolo <kache2692@gmail.com>
2019-02-04 17:21:19 -0800

    Added debounced push button to start/stop counter.

--- src/Seven_Segment.v
+++ src/Seven_Segment.v
@@ -22,16 +22,19 @@ module Seven_Segment(
     input CLK,
     input switch_up,
     input switch_clear,
+	 input switch_stop,
     output [7:0] SEG,
     output [3:0] DIGIT,
 	 output reg [23:0] io_led
     );
 
-wire s_up, s_clear;
+wire s_up, s_clear, s_stop;
 reg [23:0] prescaler;
-reg direction;
+reg direction, stopped;
+
 debouncer d1(.CLK (CLK), .switch_input (switch_up), .trans_dn (s_up));
 debouncer d2(.CLK (CLK), .switch_input (switch_clear), .trans_dn (s_clear));
+debouncer d3(.CLK (CLK), .switch_input (switch_stop), .trans_dn (s_stop));
 
 reg [3:0] units, tens, hundreds, thousands;
 
@@ -42,12 +45,14 @@ display_7_seg display(.CLK (CLK),
 always @(posedge CLK)
 begin
 
+	prescaler <= prescaler + 24'd1;
+
 	if(s_up)
 	begin
 	direction = ~direction;
 	end
 
-	if (s_clear)
+	if(s_clear)
 	begin
     units <= 0;
     tens <= 0;
@@ -55,55 +60,63 @@ begin
     thousands <= 0;
 	 io_led <= 0;
 	end
+	
+	if(s_stop)
+	begin
+		stopped = ~stopped;
+	end
   
-	prescaler <= prescaler + 24'd1;
+	
 	if (prescaler == 24'd50000000) // 1 Hz
 	begin
 	prescaler <= 0;
-		if(!direction)
-		begin
-		io_led <= io_led + 24'd1;
-		units <= units + 4'd1;
-			if (units == 4'h9) 
+		if(!stopped)
 			begin
-				units <= 0;
-				tens <= tens + 4'd1;
-				if (tens == 4'h9) 
+			if(!direction)
+			begin
+			io_led <= io_led + 24'd1;
+			units <= units + 4'd1;
+				if (units == 4'h9) 
 				begin
-					tens <= 0;
-					hundreds <= hundreds + 4'd1;
-					if (hundreds == 4'h9) 
+					units <= 0;
+					tens <= tens + 4'd1;
+					if (tens == 4'h9) 
 					begin
-						hundreds <= 0;
-						thousands <= thousands + 4'd1;
-						if (thousands == 4'h9) 
+						tens <= 0;
+						hundreds <= hundreds + 4'd1;
+						if (hundreds == 4'h9) 
 						begin
-							thousands <= 0;
+							hundreds <= 0;
+							thousands <= thousands + 4'd1;
+							if (thousands == 4'h9) 
+							begin
+								thousands <= 0;
+							end
 						end
 					end
 				end
 			end
-		end
-		
-		else
-		begin
-		io_led <= io_led - 24'd1;
-		units <= units - 4'd1;
-			if (units == 4'h0) 
+			
+			else
 			begin
-				units <= 9;
-				tens <= tens - 4'd1;
-				if (tens == 4'h0) 
+			io_led <= io_led - 24'd1;
+			units <= units - 4'd1;
+				if (units == 4'h0) 
 				begin
-					tens <= 9;
-					hundreds <= hundreds - 4'd1;
-					if (hundreds == 4'h0) 
+					units <= 9;
+					tens <= tens - 4'd1;
+					if (tens == 4'h0) 
 					begin
-						hundreds <= 9;
-						thousands <= thousands - 4'd1;
-						if (thousands == 4'h0) 
+						tens <= 9;
+						hundreds <= hundreds - 4'd1;
+						if (hundreds == 4'h0) 
 						begin
-							thousands <= 9;
+							hundreds <= 9;
+							thousands <= thousands - 4'd1;
+							if (thousands == 4'h0) 
+							begin
+								thousands <= 9;
+							end
 						end
 					end
 				end
